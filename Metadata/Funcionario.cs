c13440b804afd48b1cecaa232cb39270144f0402
João Galeazzi <joaohstolf@gmail.com>
2019-06-07 16:45:46 -0300

    Adição do IEntityCRUD e da classe Funcionario no Metadata

---
+++ Metadata/Funcionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadata
+{
+    public class Funcionario : IEntityCRUD<Funcionario>
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool EhAdmin { get; set; }
+        public bool EhAtivo { get; set; }
+        
+
+        public string Atualizar(Funcionario item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Inserir(Funcionario item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Funcionario LerPorID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Funcionario> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Metadata/IEntityCrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metadata
+{
+    public interface IEntityCRUD<T>
+    {
+        string Inserir(T item);
+        string Atualizar(T item);
+        T LerPorID(int ID);
+        List<T> LerTodos();
+    }
+}
