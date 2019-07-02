f36f78e90f82ed0cb60989dd09d8b27a2caf1257
Vitor Zapellini <zapellini.vitor@gmail.com>
2019-07-02 14:31:33 -0300

    push

---
+++ DAO/CheckInDAO.cs
@@ -0,0 +1,14 @@
+﻿using Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class CheckInDAO
+    {
+        
+    }
+}
